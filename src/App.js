8783ae344787e5522447c5d681725cee0fdc95bf
mostafa <ghost.mr303@gmail.com>
2022-02-02 20:50:28 +0330

    Routes added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.scss';
+import 'swiper/swiper.min.css'
+import './assets/boxicons-2.0.7/css/boxicons.min.css'
+import { BrowserRouter, Route } from 'react-router-dom';
+
+//Components
+import Footer from './components/footer/Footer'
+import Header from './components/header/Header'
+
+//Routes
+import Routes from './config/Routes';
 
 function App() {
   return (
-    <div>
-      APP
-    </div>
+    <BrowserRouter>
+        <Route render={props => (
+          <>
+            <Header {...props}/>
+            <Routes/>
+            <Footer/>
+          </>
+        )}/>
+    </BrowserRouter>
   );
 }
 
