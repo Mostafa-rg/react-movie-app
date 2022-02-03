d2ff8c7c46069084692134a35a72e10181ce4b83
mostafa <ghost.mr303@gmail.com>
2022-02-03 11:05:01 +0330

    Header created

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,71 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import {Link, useLocation} from 'react-router-dom'
+
+//Styles
+import './header.scss'
+
+//Logo
+import logo from '../../assets/mrmovie.png'
+
+const headerNav = [
+    {
+        display: 'Home',
+        path: '/'
+    },
+    {
+        display: 'Movies',
+        path: '/movie'
+    },
+    {
+        display: 'Tv Series',
+        path: '/tv'
+    },
+]
 
 const Header = () => {
+
+    const { pathName } = useLocation()
+    const headerRef = useRef(null)
+
+    const active = headerNav.findIndex(item => item.path === pathName)
+
+    useEffect(() => {
+        const shrinkHeader = () => {
+            if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100){
+                headerRef.current.classList.add('shrink')
+            }else{
+                headerRef.current.classList.remove('shrink')
+            }
+        }
+
+        window.addEventListener('scroll', shrinkHeader)
+        return () => {
+            window.removeEventListener('scroll', shrinkHeader)
+        }
+    },[])
+
+
     return (
-        <div>
-            Header
+        <div ref={headerRef} className='header'>
+            <div className='header__wrap container'>
+                <div className='logo'>
+                    <img src={logo} alt="logo"/>
+                    <Link to="/">mrMovie</Link>
+                </div>
+                <ul className='header__nav'>
+                    {
+                        headerNav.map((item, index) => (
+                            <li key={index} className={`${index === active ? 'active' : ''}`}>
+                                <Link to={item.path}>
+                                    {item.display}
+                                </Link>
+                            </li>
+                        ))
+                    }
+                </ul>
+
+
+            </div>
         </div>
     );
 };
