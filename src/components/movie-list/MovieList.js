e9b809776dda20f4be238df8de89f324d22043e0
mostafa <ghost.mr303@gmail.com>
2022-02-04 16:19:19 +0330

    Movie lists created

---
+++ src/components/movie-list/MovieList.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+//Styles
+import './movie-list.scss';
+
+//Swiper
+import {Swiper, SwiperSlide } from 'swiper/react'
+
+//Components
+import Button from '../button/Button';
+
+//Api
+import apiConfig from '../../Services/apiConfig';
+import tmdbApi, { category } from '../../Services/tmdbApi';
+
+const MovieList = props => {
+
+    const [items, setItems] = useState([])
+
+    useEffect(() => {
+        const getList = async () => {
+            let response = null;
+            const params = {};
+
+            if(props.type !== 'similar'){
+                switch (props.category) {
+                    case category.movie:
+                        response = await tmdbApi.getMoviesList(props.type, {params});
+                        break;
+                
+                    default:
+                        response = await tmdbApi.getTvList(props.type, {params});
+                }
+            }else{
+                response = await tmdbApi.similar(props.category, props.id);
+            }
+            setItems(response.results)
+
+        };
+        getList();
+    }, [])
+
+    return (
+        <div className='movie-list'>
+            <Swiper
+                grabCursor={true}
+                spaceBetween={10}
+                slidesPerView={'auto'}
+            >
+                {
+                    items.map((item, index) => (
+                        <SwiperSlide key={index}>
+                            <img src={apiConfig.w500Image(item.poster_path)} alt=''/>
+                        </SwiperSlide>
+                    ))
+                }
+            </Swiper>
+        </div>
+    );
+};
+
+MovieList.propTypes = {
+    category: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired
+};
+
+export default MovieList;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,10 @@ import { Link } from 'react-router-dom'
 //Component
 import HeroSlide from '../components/hero-slide/HeroSlide';
 import { OutlineButton } from '../components/button/Button';
+import MovieList from '../components/movie-list/MovieList';
+
+//Api
+import { category, movieType, tvType } from '../Services/tmdbApi';
 
 const Home = () => {
     return (
@@ -17,8 +21,38 @@ const Home = () => {
                             <OutlineButton className='small'>View more</OutlineButton>
                         </Link>
                     </div>
+                    <MovieList category={category.movie} type={movieType.popular}/>
                 </div>
 
+                <div className='section mb-3'>
+                    <div className='section__header mb-2'>
+                        <h2>Top Rated Movies</h2>
+                        <Link to='/movie'>
+                            <OutlineButton className='small'>View more</OutlineButton>
+                        </Link>
+                    </div>
+                    <MovieList category={category.movie} type={movieType.top_rated}/>
+                </div>
+
+                <div className='section mb-3'>
+                    <div className='section__header mb-2'>
+                        <h2>Trending Tv</h2>
+                        <Link to='/tv'>
+                            <OutlineButton className='small'>View more</OutlineButton>
+                        </Link>
+                    </div>
+                    <MovieList category={category.tv} type={tvType.popular}/>
+                </div>
+
+                <div className='section mb-3'>
+                    <div className='section__header mb-2'>
+                        <h2>Top Rated Tv</h2>
+                        <Link to='/tv'>
+                            <OutlineButton className='small'>View more</OutlineButton>
+                        </Link>
+                    </div>
+                    <MovieList category={category.tv} type={tvType.top_rated}/>
+                </div>
             </div>
         
         </>
